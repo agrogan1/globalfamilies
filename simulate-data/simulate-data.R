a882e3517378f8b2047dba8f907c2b713aef9304
Andrew Grogan-Kaylor <agrogan@umich.edu>
2023-06-14 18:18:13 -0400

    updates

--- simulate-data/simulate-data.R
+++ simulate-data/simulate-data.R
@@ -1,8 +1,18 @@
 # simulate MICS data
 
+# call libraries
+
+library(labelled) # labels
+
+library(haven) # write Stata
+
+# setup
+
 set.seed(1234) # random seed
 
 N <- 100 # sample size
+
+# based on MICS
 
 cd1 <- rbinom(N, 1, .38) # spank
 
@@ -12,17 +22,31 @@ cd3 <- rbinom(N, 1, .64) # shout
 
 cd4 <- rbinom(N, 1, .78) # explain
 
-z <- .7 + # based on MICS
+z <- .7 + 
   .23 * cd1 + 
   .52 * cd2 + 
   .42 * cd3 + 
   -.21 * cd4 
 
-p <- exp(z) / (1 + exp(z))
+p <- exp(z) / (1 + exp(z)) # probability
 
-aggression <- rbinom(N, 1, p) # gets along
+aggression <- rbinom(N, 1, p) # binomial y
+
+# data frame
 
 MICSsimulated <- data.frame(cd1, cd2, cd3, cd4, aggression)
+
+var_label(MICSsimulated$cd1) <- "spank"
+
+var_label(MICSsimulated$cd2) <- "beat"
+
+var_label(MICSsimulated$cd3) <- "shout"
+
+var_label(MICSsimulated$cd4) <- "explain"
+
+var_label(MICSsimulated$aggression) <- "aggression"
+
+# logistic regression model
 
 fit1 <- glm(aggression ~ cd1 + cd2 + cd3 + cd4 , 
             family = "binomial",
@@ -30,5 +54,9 @@ fit1 <- glm(aggression ~ cd1 + cd2 + cd3 + cd4 ,
 
 summary(fit1)
 
+# write data to various formats
 
+save(MICSsimulated, file = "./simulate-data/MICSsimulated.RData")
+
+write_dta(MICSsimulated, "./simulate-data/MICSsimulated.dta")
 
